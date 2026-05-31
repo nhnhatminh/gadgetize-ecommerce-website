b95288acbe9360d1f7392f9a5d4cfb493d3b608f
Nguyen Huynh Nhat Minh <nhnm0609@gmail.com>
2026-06-01 01:48:31 +0700

    feat: extract RelatedProducts component with Swiper slider carousel

---
+++ src/components/productDetail/RelatedProducts.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Pagination } from "swiper/modules";
+import ProductCard from "../common/ProductCard";
+
+import "swiper/css";
+import "swiper/css/pagination";
+
+export default function RelatedProducts({ relatedProducts }) {
+  return (
+    <div className="row mt-5">
+      <div className="col-12">
+        <div className="related-products-wrapper mb-5">
+          <h4 className="fw-bold mb-4">Sản Phẩm Gợi Ý</h4>
+          <Swiper
+            modules={[Autoplay, Pagination]}
+            spaceBetween={24}
+            loop={true}
+            pagination={{ clickable: true }}
+            autoplay={{ delay: 3000, disableOnInteraction: false }}
+            breakpoints={{
+              0: { slidesPerView: 2 },
+              576: { slidesPerView: 3 },
+              768: { slidesPerView: 4 },
+              992: { slidesPerView: 5 },
+              1200: { slidesPerView: 6 },
+            }}
+            className="related-products-swiper"
+          >
+            {relatedProducts.map((prod) => (
+              <SwiperSlide key={prod.id}>
+                <ProductCard product={prod} layoutMode="vertical" />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
+      </div>
+    </div>
+  );
+}
