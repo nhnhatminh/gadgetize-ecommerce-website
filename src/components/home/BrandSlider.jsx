f5ea17f6bfbfc61f0b3dadb9e14a6292ee495be2
Nguyen Huynh Nhat Minh <nhnm0609@gmail.com>
2026-05-31 22:28:11 +0700

    feat: extract BrandSlider component for partner logos carousel

---
+++ src/components/home/BrandSlider.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper/modules";
+
+import "swiper/css";
+
+export default function BrandSlider() {
+  return (
+    <section className="brands-section py-5 border-top bg-light">
+      <div className="container">
+        <Swiper
+          modules={[Autoplay]}
+          spaceBetween={30}
+          loop={true}
+          autoplay={{ delay: 3000, disableOnInteraction: false }}
+          breakpoints={{
+            0: { slidesPerView: 2 },
+            576: { slidesPerView: 3 },
+            992: { slidesPerView: 4 },
+            1200: { slidesPerView: 5 },
+          }}
+        >
+          {[1, 2, 3, 4, 5].map((num) => (
+            <SwiperSlide key={num}>
+              <div
+                className="brand-item text-center p-3 bg-white rounded-3 border d-flex align-items-center justify-content-center"
+                style={{ height: "80px" }}
+              >
+                <img
+                  src={`/images/brand-${num}.png`}
+                  alt={`Brand ${num}`}
+                  className="img-fluid"
+                  style={{ maxHeight: "70%" }}
+                />
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </section>
+  );
+}
